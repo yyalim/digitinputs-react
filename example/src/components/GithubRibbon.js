b12a0c28d38b46570356b15433ffd318eb51f61c
Yusuf Yalim <yusufyalim@gmail.com>
2021-01-18 16:43:59 +0100

    Use className instead of class

--- example/src/components/GithubRibbon.js
+++ example/src/components/GithubRibbon.js
@@ -10,7 +10,7 @@ export default function GithubRibbon() {
         width='149'
         height='149'
         src='https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149'
-        class='attachment-full size-full'
+        className='attachment-full size-full'
         alt='Fork me on GitHub'
         data-recalc-dims='1'
       />
